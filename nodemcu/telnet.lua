e45cfdbc3f9ddd93f443dcc35367d3ef2cceae61
Shun Git <kwokshungit@gmail.com>
2015-09-05 22:45:13 +0800

    update telnet

--- nodemcu/telnet.lua
+++ nodemcu/telnet.lua
@@ -1,7 +1,7 @@
-srv:listen(23,function(c)
+stcp:listen(23,function(c)
  print("WiFi console connected")
- function sput(str) if (c~=nil) then c:send(str) end end
- node.output(sput,1)
+ function spt(s) if (c~=nil) then c:send(s) end end
+ node.output(spt,1)
  c:on("receive",function(c,l) node.input(l) end)
  c:on("disconnection",function(c) node.output(nil) end)
 end)
